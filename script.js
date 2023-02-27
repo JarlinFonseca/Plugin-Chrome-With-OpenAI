27be67aa6ac229f66120d9f937d10ee80e0775c1
Jarlin Andres Fonseca Bermon <jarlinandres5000@gmail.com>
2023-02-26 22:01:40 -0500

    Se validó el input mostrando el error cuando se deja vacio el campo, se arreglaron detalles en la vista y se implemento efecto de cargando antes de cargar el contenido fetch API

--- script.js
+++ script.js
@@ -2,40 +2,55 @@ document.addEventListener('DOMContentLoaded', function () {
 
   try {
     const prompt = document.getElementById('prompt');
-    const convert = document.getElementById('convert');
+
+    const generate = document.getElementById('generate');
+
+    generate.onclick = function () {
+      if (prompt.value.trim() == ""){ 
+        alert("You must enter a prompt in the field (message that generates the desired image).");
+        document.getElementById('aviso').style.display = "block";
+        document.getElementById('aviso').innerHTML ="You have not entered any message."
+      }else document.getElementById('aviso').style.display = "none";
+    };
     const result = document.getElementById('result');
 
     // const apiKey = "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"
     //const apiUrl = "https://api.openai.com/v1/images/generations"
 
-    convert.addEventListener('click', async () => {
-      const promptR = prompt.value;
+    generate.addEventListener('click', async () => {
+      const promptR = prompt.value.trim();
 
+      console.log(promptR)
+      if (promptR != null && promptR != "") {
+        document.getElementById("spinner").style.display="block";
+        document.getElementById("show").style.display="none";
+        const response = await fetch('https://api.openai.com/v1/images/generations', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer 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'
+          },
+          body: JSON.stringify({
+            "prompt": promptR,
+            "n": 1,
+            "size": "1024x1024"
+          })
+        });
+       
+        const data = await response.json();
+              // línea ocultando el spinner
+    document.getElementById("spinner").style.display="none";
 
-      const response = await fetch('https://api.openai.com/v1/images/generations', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer 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'
-        },
-        body: JSON.stringify({
-          "prompt": promptR,
-          //"prompt": "A cute baby sea otter",
-          "n": 1,
-          "size": "1024x1024"
-        })
-      });
-      const data = await response.json();
-      console.log(data)
-      image_url = data.data[0].url;
+        console.log(data)
+        image_url = data.data[0].url;
 
-      document.getElementById("show").style.display = "block";
+        document.getElementById("show").style.display = "block";
 
-      document.getElementById("image").src = image_url;
+        document.getElementById("image").src = image_url;
 
-      //document.getElementById("lnkPrueba").href = image_url;
-
-       result.result = `${image_url} `;
+        //document.getElementById("result").href = image_url;
+        result.href = `${image_url} `;
+      }
 
     })
 
